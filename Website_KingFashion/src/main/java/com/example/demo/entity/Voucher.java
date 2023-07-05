f5aacdb84241ffe9fd9088383dbb3db9319907a8
dungtq-ph23717 <118000843+dungtq-ph23717@users.noreply.github.com>
2023-07-05 20:17:54 +0700

    dung

--- Website_KingFashion/src/main/java/com/example/demo/entity/Voucher.java
+++ Website_KingFashion/src/main/java/com/example/demo/entity/Voucher.java
@@ -1,9 +1,19 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
