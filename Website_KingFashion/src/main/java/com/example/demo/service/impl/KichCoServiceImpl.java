6b7cee9559aa3f9bd790a00ec3de06d55876f146
NamPH23205 <namtvph23205@fpt.edu.vn>
2023-07-11 18:59:09 +0700

    KichCo

---
+++ Website_KingFashion/src/main/java/com/example/demo/service/impl/KichCoServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.KichCo;
+import com.example.demo.repository.KichCoRepository;
+import com.example.demo.service.KichCoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class KichCoServiceImpl implements KichCoService {
+
+    @Autowired
+    private KichCoRepository kichCoRepository;
+
+    @Override
+    public Page<KichCo> phanTrang(Integer pageNum, Integer pageNo) {
+        Pageable pageable = PageRequest.of(pageNum,pageNo);
+        return kichCoRepository.findAll(pageable);
+    }
+
+    @Override
+    public void add(KichCo kichCo) {
+            kichCoRepository.save(kichCo);
+    }
+
+    @Override
+    public KichCo detail(UUID id) {
+        return kichCoRepository.getKichCoById(id);
+    }
+
+    @Override
+    public void delete(UUID id) {
+            kichCoRepository.deleteById(id);
+    }
+}
