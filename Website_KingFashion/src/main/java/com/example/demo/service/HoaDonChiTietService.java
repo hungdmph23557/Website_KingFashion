fc2bff5f02d70efe28aea5f4c441017613e67306
dathn963 <nguyenvandathn963@gmail.com>
2023-07-23 23:44:18 +0700

    hoadon
    aaaaa

---
+++ Website_KingFashion/src/main/java/com/example/demo/service/HoaDonChiTietService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.HoaDonChiTiet;
+
+import java.util.List;
+
+public interface HoaDonChiTietService {
+
+    List<HoaDonChiTiet> getALl();
+}
