1cfba405b83c93b2a57148c50a534a23447fd83b
dathn963 <nguyenvandathn963@gmail.com>
2023-07-28 00:27:53 +0700

    backendhoadon

--- Website_KingFashion/src/main/java/com/example/demo/service/HoaDonService.java
+++ Website_KingFashion/src/main/java/com/example/demo/service/HoaDonService.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.HoaDon;
 
 import org.springframework.data.domain.Page;
 
+import java.util.Date;
 import java.util.List;
 
 public interface HoaDonService {
@@ -11,6 +12,8 @@ public interface HoaDonService {
     List<HoaDon> getAll();
 
     Page<HoaDon> phanTrangHoaDon(Integer pageNum, Integer pageNo);
+
+    Page<HoaDon> search(String maHoaDon, Date ngayThanhToan, Double tongTienSauKhiGiam, Boolean trangThai, String tenNguoiNhan, Date ngayNhanDuKien, Date ngayShip, Integer size, Integer page);
 
     List<HoaDon> getExcel();
 }
